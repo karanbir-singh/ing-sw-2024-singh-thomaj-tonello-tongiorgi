aaf9ee30aa7794432723f601772907dc029292ad
KevinThomaj <kevin.thomaj02@gmail.com>
2024-03-13 11:46:33 +0100

    Tutto Sistemato con gli optional

--- src/main/java/it/polimi/ingsw/gc26/PersonalBoard.java
+++ src/main/java/it/polimi/ingsw/gc26/PersonalBoard.java
@@ -28,8 +28,8 @@ public class PersonalBoard {
     public boolean checkIfPlayable(int x, int y){
         return ifPresent(x,y,playablePositions).isPresent();
     }
-    public void playSide(int x, int y, Side side){//supponiamo sia una posizione valida
-        Point p = ifPresent(x,y, playablePositions).orElseThrow(() ->(new nullPointerException());
+    public void playSide(int x, int y, Side side) throws nullPointerException{//supponiamo sia una posizione valida
+        Point p = ifPresent(x,y, playablePositions).orElseThrow(() ->(new nullPointerException()));
         p.setSide(side);
         movePoint(x,y,occupiedPositions, playablePositions);
 
@@ -37,87 +37,86 @@ public class PersonalBoard {
 
 
         //begin analyzing the point X+1,Y+1
-        if(ifPresent(x+1,y+1,occupiedPositions) != null){
+        if(ifPresent(x+1,y+1,occupiedPositions).isPresent()){
             Point p = ifPresent(x+1,y+1, occupiedPositions);
             personalBoardSymbols.decreaseResource(p.side.DOWNLEFT.resource);
             p.side.DOWNLEFT.isHidden = true;
-        }else if(ifPresent(x+1,y+1,blockedPositions) != null){
-        }else if(ifPresent(x+1,y+1,playablePositions) != null) {
+        }else if(ifPresent(x+1,y+1,blockedPositions).isPresent()){
+        }else if(ifPresent(x+1,y+1,playablePositions).isPresent()) {
         }else
         if(side.UPRIGHT.evil == true){
             addPoint(x+1,y+1,blockedPositions);
-        }else{
-            addPoint(x+1,y+1, playablePositions);
+        }else {
+            addPoint(x + 1, y + 1, playablePositions);
         }
-    }
     //end analyzing the point X+1, Y+1
 
     //begin analyzing the point X-1,Y+1
-    if(ifPresent(x-1,y+1,occupiedPositions) != null){
-        Point p = ifPresent(x-1,y+1, occupiedPositions);
-        personalBoardSymbols.decreaseResource(p.side.DOWNRIGHT.resource);
-        p.side.DOWNRIGHT.isHidden = true;
-    }else if(ifPresent(x-1,y+1,blockedPositions) != null){
-    }else if(ifPresent(x-1,y+1,playablePositions) != null){
-    }else{
-        if(side.UPLEFT.evil == true){
-            addPoint(x-1,y+1,blockedPositions);
+        if(ifPresent(x-1,y+1,occupiedPositions).isPresent()){
+            Point p = ifPresent(x-1,y+1, occupiedPositions).orElseThrow(() ->(new nullPointerException());
+            personalBoardSymbols.decreaseResource(p.side.DOWNRIGHT.resource);
+            p.side.DOWNRIGHT.isHidden = true;
+        }else if(ifPresent(x-1,y+1,blockedPositions).isPresent()){
+        }else if(ifPresent(x-1,y+1,playablePositions).isPresent()){
         }else{
-            addPoint(x-1,y+1, playablePositions);
+            if(side.UPLEFT.evil == true){
+                addPoint(x-1,y+1,blockedPositions);
+            }else{
+                addPoint(x-1,y+1, playablePositions);
+            }
         }
-    }
     //end analyzing the point X-1, Y+1
 
 
     //begin analyzing the point X-1,Y-1
-    if(ifPresent(x-1,y-1,occupiedPositions) != null){
-        Point p = ifPresent(x-1,y-1, occupiedPositions);
-        personalBoardSymbols.decreaseResource(p.side.UPRIGHT.resource);
-        p.side.UPRIGHT.isHidden = true;
-    }else if(ifPresent(x-1,y-1,blockedPositions) != null){
-    }else if(ifPresent(x-1,y-1,playablePositions) != null){
-    }else{
-        if(side.DOWNLEFT.evil == true){
-            addPoint(x-1,y-1,blockedPositions);
+        if(ifPresent(x-1,y-1,occupiedPositions).isPresent()){
+            Point p = ifPresent(x-1,y-1, occupiedPositions).orElseThrow(() ->(new nullPointerException());
+            personalBoardSymbols.decreaseResource(p.side.UPRIGHT.resource);
+            p.side.UPRIGHT.isHidden = true;
+        }else if(ifPresent(x-1,y-1,blockedPositions).isPresent()){
+        }else if(ifPresent(x-1,y-1,playablePositions).isPresent()){
         }else{
-            addPoint(x-1,y-1, playablePositions);
+            if(side.DOWNLEFT.evil == true){
+                addPoint(x-1,y-1,blockedPositions);
+            }else{
+                addPoint(x-1,y-1, playablePositions);
+            }
         }
-    }
-    //end analyzing the point X-1, Y-1
+        //end analyzing the point X-1, Y-1
 
-    //begin analyzing the point X+1,Y-1
-    if(ifPresent(x+1,y-1,occupiedPositions) != null){
-        Point p = ifPresent(x+1,y-1, occupiedPositions);
-        personalBoardSymbols.decreaseResource(p.side.UPLEFT.resource);
-        p.side.UPLEFT.isHidden = true;
-    }else if(ifPresent(x+1,y-1,blockedPositions) != null){
-    }else if(ifPresent(x+1,y-1,playablePositions) != null){
-    }else{
-        if(side.DOWNRIGHT.evil == true){
-            addPoint(x+1,y-1,blockedPositions);
+        //begin analyzing the point X+1,Y-1
+        if(ifPresent(x+1,y-1,occupiedPositions).isPresent()){
+            Point p = ifPresent(x+1,y-1, occupiedPositions).orElseThrow(() ->(new nullPointerException());
+            personalBoardSymbols.decreaseResource(p.side.UPLEFT.resource);
+            p.side.UPLEFT.isHidden = true;
+        }else if(ifPresent(x+1,y-1,blockedPositions).isPresent()){
+        }else if(ifPresent(x+1,y-1,playablePositions).isPresent(){
         }else{
-            addPoint(x+1,y-1, playablePositions);
+            if(side.DOWNRIGHT.evil == true){
+                addPoint(x+1,y-1,blockedPositions);
+            }else{
+                addPoint(x+1,y-1, playablePositions);
+            }
         }
-    }
-    //end analyzing the point X+1, Y-1
+        //end analyzing the point X+1, Y-1
 
 
 
 
-    //addSymbol
+        //addSymbol
         for(int i = 0; i < side.resource.length(); i++){
-        personalBoardSymbols.increaseResource(side.resource.get(i));
-    }
+            personalBoardSymbols.increaseResource(side.resource.get(i));
+        }
         this.incrementResource(side.UPRIGHT.resource);
         this.incrementResource(side.UPLEFT.resource);
         this.incrementResource(side.DOWNLEFT.resource);
         this.incrementResource(side.DOWNRIGHT.resource);
 
-    //adding points of the card played
-    this.score = this.score + side.points;
+        //adding points of the card played
+        this.score = this.score + side.points;
 
-    //adding points of the cart ability
-    this.score = this.score + side.useAbility(occupiedPositions, personalBoardSymbols.visibleResouces);
+        //adding points of the cart ability
+        this.score = this.score + side.useAbility(occupiedPositions, personalBoardSymbols.visibleResouces);
 
 }
 private Optional<Point> ifPresent(int x, int y, ArrayList<Point> l){
