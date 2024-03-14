20c4519212845367556d56a11a2d0ffae9d605f6
Gabi <gabriele.tonello@mail.polimi.it>
2024-03-14 11:12:01 +0100

    Continuo specifiche json

---
+++ src/main/java/it/polimi/ingsw/gc26/Parser/ParserCore.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.gc26.Parser;
+import java.util.*;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+public class ParserCore {
+
+}
