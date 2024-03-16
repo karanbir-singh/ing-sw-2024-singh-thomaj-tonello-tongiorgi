f75ea4a31fe4d896474eead263655f1965342c83
Gabi <gabriele.tonello@mail.polimi.it>
2024-03-16 14:45:11 +0100

    First version Parser

--- src/main/java/it/polimi/ingsw/gc26/Main.java
+++ src/main/java/it/polimi/ingsw/gc26/Main.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.gc26;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import it.polimi.ingsw.gc26.Parser.ParserCore;
+
 public class Main {
     public static void main(String[] args) {
         // This is a main class: for now is useless
+        ParserCore p = new ParserCore("src/main/resources/Data/CodexNaturalisCards.json");
+        JsonNode p1 = p.getGoldCards();
+        System.out.println(p1);
     }
 }
--- src/main/java/it/polimi/ingsw/gc26/Parser/ParserCore.java
+++ src/main/java/it/polimi/ingsw/gc26/Parser/ParserCore.java
@@ -1,6 +1,41 @@
 package it.polimi.ingsw.gc26.Parser;
 import com.fasterxml.jackson.databind.JsonNode;
-import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class ParserCore {
+    private final String filePath;
+    public ParserCore(String filePath) {
+        this.filePath = filePath;
+    }
+    private JsonNode getRootObject() {
+        try {
+            ObjectMapper JsonMapper = new ObjectMapper();
+            return JsonMapper.readTree(new FileReader(this.filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public JsonNode getStarterCards() {
+        return this.getRootObject().get("StarterCards");
+    }
+
+    public JsonNode getObjectiveCards() {
+        return getRootObject().get("ObjectiveCards");
+    }
+
+    public JsonNode getResourceCards() {
+        return getRootObject().get("ResourceCards");
+    }
+
+    public JsonNode getGoldCards() {
+        return getRootObject().get("GoldCards");
+    }
+
 }
+
+
