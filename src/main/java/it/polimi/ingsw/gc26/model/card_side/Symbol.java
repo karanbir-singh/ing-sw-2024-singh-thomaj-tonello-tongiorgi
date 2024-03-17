c7fe62889d5ccd0f570a8e2d019a325f641abe68
karan <karanbir.jsm@gmail.com>
2024-03-17 11:42:18 +0100

    Rewrite symbols

--- src/main/java/it/polimi/ingsw/gc26/model/card_side/Symbol.java
+++ src/main/java/it/polimi/ingsw/gc26/model/card_side/Symbol.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.gc26.model.card_side;
 
 public enum Symbol {
-    MUSHROOM, WOLF, LEAF, BUTTERFLY, INKWELL, QUILL, MANUSCRIPT;
+    FUNGI, ANIMAL, PLANT, INSECT, INKWELL, QUILL, MANUSCRIPT;
 }
