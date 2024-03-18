f2e89c0760a2a76e95cf2b5e19ff5db113b4cf71
karan <karanbir.jsm@gmail.com>
2024-03-18 09:35:13 +0100

    Add constructor

--- src/main/java/it/polimi/ingsw/gc26/model/card_side/ResourceCardFront.java
+++ src/main/java/it/polimi/ingsw/gc26/model/card_side/ResourceCardFront.java
@@ -1,11 +1,22 @@
 package it.polimi.ingsw.gc26.model.card_side;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class ResourceCardFront extends Side {
-    public ResourceCardFront(Optional<Symbol> sideSymbol, ArrayList<Symbol> permanentResources, Map<Symbol, Integer> requestedResources, Corner UPLEFT, Corner DOWNLEFT, Corner UPRIGHT, Corner DOWNRIGHT) {
-        super(sideSymbol, permanentResources, requestedResources, UPLEFT, DOWNLEFT, UPRIGHT, DOWNRIGHT);
+
+    public ResourceCardFront(Symbol sideSymbol, int points, Corner UPLEFT, Corner DOWNLEFT, Corner UPRIGHT, Corner DOWNRIGHT) {
+        setSideSymbol(sideSymbol);
+        setPoints(points);
+        setDOWNLEFT(DOWNLEFT);
+        setDOWNRIGHT(DOWNRIGHT);
+        setUPLEFT(UPLEFT);
+        setUPRIGHT(UPRIGHT);
+
+        setRequestedResources(new HashMap<>());
+        setPermanentResources(new ArrayList<>());
+        setType(0);
     }
 }
