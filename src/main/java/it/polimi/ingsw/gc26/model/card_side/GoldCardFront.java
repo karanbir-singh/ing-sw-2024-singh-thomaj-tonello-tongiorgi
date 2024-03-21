5b8d07f2dba3f82a9f615f8b42c5331dedb99a85
karan <karanbir.jsm@gmail.com>
2024-03-21 19:57:47 +0100

    Fix useAbility

--- src/main/java/it/polimi/ingsw/gc26/model/card_side/GoldCardFront.java
+++ src/main/java/it/polimi/ingsw/gc26/model/card_side/GoldCardFront.java
@@ -1,11 +1,8 @@
 package it.polimi.ingsw.gc26.model.card_side;
 
-import it.polimi.ingsw.gc26.model.player.Point;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 public class GoldCardFront extends Side {
     public GoldCardFront(Symbol sideSymbol, Map<Symbol, Integer> requestedResources, int points, Corner UPLEFT, Corner DOWNLEFT, Corner UPRIGHT, Corner DOWNRIGHT) {
@@ -19,10 +16,5 @@ public class GoldCardFront extends Side {
 
         setPermanentResources(new ArrayList<>());
         setType(0);
-    }
-
-    @Override
-    public int useAbility(Map<Symbol, Integer> resources, ArrayList<Point> occupiedPositions, Point p) {
-        return getPoints();
     }
 }
