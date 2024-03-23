5bdb1f2774bc5be50b95c8f255616f95c9c6bf09
Gabi <gabriele.tonello@mail.polimi.it>
2024-03-23 16:39:22 +0100

    StarterCards test

--- src/test/java/it/polimi/ingsw/gc26/Parser/ParserCoreTest.java
+++ src/test/java/it/polimi/ingsw/gc26/Parser/ParserCoreTest.java
@@ -1,8 +1,12 @@
 package it.polimi.ingsw.gc26.Parser;
 
 import it.polimi.ingsw.gc26.model.card.Card;
+import it.polimi.ingsw.gc26.model.card_side.Symbol;
 import it.polimi.ingsw.gc26.model.deck.Deck;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +18,14 @@ class ParserCoreTest {
     void getStarterCards() {
         Deck starterDeck = parser.getStarterCards();
         Card firstCard = starterDeck.removeCard();
+        assertEquals(firstCard.getFront().getUPRIGHT().getSymbol(), Optional.of(Symbol.PLANT));
+        assertEquals(firstCard.getFront().getUPLEFT().getSymbol(), Optional.empty());
+        assertEquals(firstCard.getFront().getDOWNRIGHT().getSymbol(), Optional.empty());
+        assertEquals(firstCard.getFront().getDOWNLEFT().getSymbol(), Optional.of(Symbol.INSECT));
+        assertEquals(firstCard.getFront().getPoints(), 0);
+        assertTrue(firstCard.getFront().getRequestedResources().isEmpty());
+        assertTrue(firstCard.getFront().getPermanentResources().isEmpty());
+        assertEquals(0, firstCard.getFront().getType());
 
     }
 
@@ -21,6 +33,7 @@ class ParserCoreTest {
     void getMissionCards() {
         Deck missionDeck = parser.getMissionCards();
         Card firstCard = missionDeck.removeCard();
+
     }
 
     @Test
