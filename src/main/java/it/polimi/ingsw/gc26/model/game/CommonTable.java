0dcbfa0245aac5fac2d9440a8813cd90c608d168
karan <karanbir.jsm@gmail.com>
2024-03-23 19:05:03 +0100

    Rename and add getters

--- src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
+++ src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
@@ -1,24 +1,24 @@
 package it.polimi.ingsw.gc26.model.game;
 
 import it.polimi.ingsw.gc26.model.card.Card;
-import it.polimi.ingsw.gc26.model.card.MissionCard;
 import it.polimi.ingsw.gc26.model.deck.Deck;
 
 import java.util.*;
+
 public class CommonTable {
     private final Deck resourceDeck;
     private final Deck goldDeck;
     private final Deck initialDeck;
     private final Deck missionDeck;
     private final ArrayList<Card> commonMissions;
-    private final ArrayList<Card> resourceCardsOnTable;
-    private final ArrayList<Card> goldCardsOnTable;
+    private final ArrayList<Card> resourceCards;
+    private final ArrayList<Card> goldCards;
     private Optional<Card> selectedCard;
 
-    public CommonTable(Deck resourceDeck, Deck goldDeck, Deck initialDeck, Deck missionDeck){
+    public CommonTable(Deck resourceDeck, Deck goldDeck, Deck initialDeck, Deck missionDeck) {
         commonMissions = new ArrayList<>();
-        resourceCardsOnTable = new ArrayList<>();
-        goldCardsOnTable = new ArrayList<>();
+        resourceCards = new ArrayList<>();
+        goldCards = new ArrayList<>();
         this.resourceDeck = resourceDeck;
         this.goldDeck = goldDeck;
         this.initialDeck = initialDeck;
@@ -26,46 +26,49 @@ public class CommonTable {
         selectedCard = Optional.empty();
     }
 
-    public void selectCard(Card cardSelected){ //TODO controllare se carta e nel top del deck
+    public void selectCard(Card cardSelected) {
         this.selectedCard = Optional.of(cardSelected);
     }
 
-    public void addCardToTable(Card card, ArrayList<Card> cards, int index){
-        cards.add(index,card);
+    // TODO controlla se servono veramente il metodo addCard e removeCard (si può passare direttamente dagli array
+    public void addCard(Card card, ArrayList<Card> cards, int index) {
+        cards.add(index, card);
     }
 
-    //passi l'array su cui fare il controllo
-    public Card removeCardFromTable(ArrayList<Card> cards, int index) {
+    public Card removeCard(ArrayList<Card> cards, int index) {
         return cards.remove(index);
     }
 
-
-    public Card removeCardFromDeck(Deck deck){
-        return deck.removeCard();
-
-    }
-
-    public Card getSelectedCard() throws NullPointerException{
+    public Card getSelectedCard() throws NullPointerException {
         return this.selectedCard.orElseThrow(NullPointerException::new);
     }
 
-    public ArrayList<Card> getResourceCardsOnTable(){
-        return resourceCardsOnTable;
+    public ArrayList<Card> getResourceCards() {
+        return resourceCards;
     }
-    public ArrayList<Card> getGoldCardsOnTable(){
-        return goldCardsOnTable;
+
+    public ArrayList<Card> getGoldCards() {
+        return goldCards;
     }
-    public ArrayList<Card> getCommonMissions(){
+
+    public ArrayList<Card> getCommonMissions() {
         return new ArrayList<>(commonMissions);
     }
 
-    public Deck getResourceDeck(){
+    public Deck getResourceDeck() {
         return resourceDeck;
     }
 
 
-    public Deck getGoldDeck(){
+    public Deck getGoldDeck() {
         return goldDeck;
     }
 
+    public Deck getInitialDeck() {
+        return initialDeck;
+    }
+
+    public Deck getMissionDeck() {
+        return missionDeck;
+    }
 }
