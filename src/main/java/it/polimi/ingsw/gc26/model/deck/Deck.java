62d1faf15d95b58a49433a14ead8db6f215f9612
karan <karanbir.jsm@gmail.com>
2024-03-24 21:51:57 +0100

    Rename methods and delete useless comments

--- src/main/java/it/polimi/ingsw/gc26/model/deck/Deck.java
+++ src/main/java/it/polimi/ingsw/gc26/model/deck/Deck.java
@@ -12,22 +12,23 @@ public class Deck {
         this.cards = new ArrayList<>();
     }
 
-    public void addCard(Card card){
+    public void addCard(Card card) {
         cards.add(card);
     }
 
-    /* the last card of the Deck is saved in drawnCard, then removed from the Deck and return to the caller */
-    public Card getTopCard(){
+    public Card getTopCard() {
         return cards.getLast();
     }
-    public Card removeCard(){
+
+    public Card removeCard() {
         return cards.removeLast();
     }
 
-    public void shuffleDeck(){
+    public void shuffleDeck() {
         Collections.shuffle(cards);
     }
-    public ArrayList<Card> getDeck(){//da togliere serve per prova
+
+    public ArrayList<Card> getCards() {//da togliere serve per prova
         return this.cards;
     }
 }
