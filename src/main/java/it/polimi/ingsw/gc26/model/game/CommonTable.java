f0f9f9b6d2a0bafab79fda6ee1ceb580421715b9
Gabi <gabriele.tonello@mail.polimi.it>
2024-03-29 19:12:13 +0100

    Javadoc to Game

--- src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
+++ src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
@@ -5,16 +5,50 @@ import it.polimi.ingsw.gc26.model.deck.Deck;
 
 import java.util.*;
 
+/**
+ * This class represents a table containing decks every player can interact with
+ */
 public class CommonTable {
+    /**
+     * This attribute represents the resource cards deck
+     */
     private final Deck resourceDeck;
+    /**
+     * This attribute represents the gold cards deck
+     */
     private final Deck goldDeck;
+    /**
+     * This attribute represents the initial cards decl
+     */
     private final Deck initialDeck;
+    /**
+     * This attribute represents the mission cards deck
+     */
     private final Deck missionDeck;
+    /**
+     * This attribute represents the two common mission that every player has
+     */
     private final ArrayList<Card> commonMissions;
+    /**
+     * This attribute represents the two resource cards that are visible in the table
+     */
     private final ArrayList<Card> resourceCards;
+    /**
+     * This attribute represents the two gold cards that are visible in the table
+     */
     private final ArrayList<Card> goldCards;
+    /**
+     * Card selected by the player
+     */
     private Card selectedCard;
 
+    /**
+     * Initializes the common table with the decks
+     * @param resourceDeck resources cards deck
+     * @param goldDeck gold cards deck
+     * @param initialDeck initial cards deck
+     * @param missionDeck mission cards deck
+     */
     public CommonTable(Deck resourceDeck, Deck goldDeck, Deck initialDeck, Deck missionDeck) {
         commonMissions = new ArrayList<>();
         resourceCards = new ArrayList<>();
@@ -26,48 +60,96 @@ public class CommonTable {
         selectedCard = null;
     }
 
+    /**
+     * Sets the attribute selectedCard to the given paramenter
+     * @param cardSelected new card selected
+     */
     public void selectCard(Card cardSelected) {
         this.selectedCard = cardSelected;
     }
 
     // TODO controlla se servono veramente il metodo addCard e removeCard (si può passare direttamente dagli array
-    public void addCard(Card card, ArrayList<Card> cards, int index) {
-        cards.add(index, card);
+
+    /**
+     * Adds the card to the deck given in the position given
+     * @param card new card to be added
+     * @param deck deck where the card will be added
+     * @param index position where the card will be added
+     */
+    public void addCard(Card card, ArrayList<Card> deck, int index) {
+        deck.add(index, card);
     }
 
-    public Card removeCard(ArrayList<Card> cards, int index) {
-        return cards.remove(index);
+    /**
+     * Removes a card from a deck and returns the card
+     * @param deck deck where the card will be removed
+     * @param index position where the card to be removed is currently
+     * @return card removed
+     */
+    public Card removeCard(ArrayList<Card> deck, int index) {
+        return deck.remove(index);
     }
 
+    /**
+     * Returns the card selected by the player
+     * @return selected card
+     */
     public Optional<Card> getSelectedCard() {
         return Optional.ofNullable(this.selectedCard);
     }
 
+    /**
+     * Returns the Resources cards visible
+     * @return resource cards visible
+     */
     public ArrayList<Card> getResourceCards() {
         return resourceCards;
     }
 
+    /**
+     * Returns the Gold cards visible
+     * @return gold cards visible
+     */
     public ArrayList<Card> getGoldCards() {
         return goldCards;
     }
 
+    /**
+     * Return the common missions
+     * @return common mission
+     */
     public ArrayList<Card> getCommonMissions() {
         return new ArrayList<>(commonMissions);
     }
 
+    /**
+     * Returns the resource cards deck
+     * @return resource cards deck
+     */
     public Deck getResourceDeck() {
         return resourceDeck;
     }
 
-
+    /**
+     * Returns the gold cards deck
+     * @return gold cards deck
+     */
     public Deck getGoldDeck() {
         return goldDeck;
     }
 
+    /**
+     * Returns the initial cards deck
+     * @return initial cards deck
+     */
     public Deck getInitialDeck() {
         return initialDeck;
     }
 
+    /**
+     * Returns the mission cards deck
+     * @return mission cards deck
+     */
     public Deck getMissionDeck() {
         return missionDeck;
     }
