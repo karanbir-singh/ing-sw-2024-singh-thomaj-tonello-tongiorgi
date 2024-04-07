d58880042787888b90264c39c00dde0ed965c691
karan <karanbir.jsm@gmail.com>
2024-04-07 23:10:04 +0200

    Change selected card into selected position (now there are saved the coordinates of the selected card)

--- src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
+++ src/main/java/it/polimi/ingsw/gc26/model/game/CommonTable.java
@@ -30,24 +30,29 @@ public class CommonTable {
      */
     private final ArrayList<Card> commonMissions;
     /**
-     * This attribute represents the two resource cards that are visible in the table
+     * This attribute represents the two resource cards that are visible on the table
      */
     private final ArrayList<Card> resourceCards;
     /**
-     * This attribute represents the two gold cards that are visible in the table
+     * This attribute represents the two gold cards that are visible on the table
      */
     private final ArrayList<Card> goldCards;
     /**
-     * Card selected by the player
+     * Coordinate X of the selected card on the table
      */
-    private Card selectedCard;
+    private int selectedX;
+    /**
+     * Coordinate Y of the selected card on the table
+     */
+    private int selectedY;
 
     /**
      * Initializes the common table with the decks
+     *
      * @param resourceDeck resources cards deck
-     * @param goldDeck gold cards deck
-     * @param starterDeck initial cards deck
-     * @param missionDeck mission cards deck
+     * @param goldDeck     gold cards deck
+     * @param starterDeck  initial cards deck
+     * @param missionDeck  mission cards deck
      */
     public CommonTable(Deck resourceDeck, Deck goldDeck, Deck starterDeck, Deck missionDeck) {
         commonMissions = new ArrayList<>();
@@ -57,49 +62,123 @@ public class CommonTable {
         this.goldDeck = goldDeck;
         this.starterDeck = starterDeck;
         this.missionDeck = missionDeck;
-        selectedCard = null;
+        selectedX = -1;
+        selectedY = -1;
     }
 
     /**
-     * Sets the attribute selectedCard to the given paramenter
-     * @param cardSelected new card selected
+     * Sets the attribute selectedX and selectedY of the chosen card to select
+     *
+     * @param selectedX coordinate X of the selected card
+     * @param selectedY coordinate Y of the selected card
      */
-    public void selectCard(Card cardSelected) {
-        this.selectedCard = cardSelected;
+    public void selectCard(int selectedX, int selectedY) {
+        // Check if the selectedX and selectedY are correct
+        if (selectedY >= 0 && selectedY < 2) {
+            if (selectedX >= 0 && selectedX < 3) {
+                this.selectedX = selectedX;
+                this.selectedY = selectedY;
+            } else {
+                // TODO gestire quando la posizione X non è corretta
+            }
+        } else {
+            // TODO gestire quando la posizione Y non è corretta
+        }
     }
 
-    // TODO controlla se servono veramente il metodo addCard e removeCard (si può passare direttamente dagli array
-
     /**
-     * Adds the card to the deck given in the position given
-     * @param card new card to be added
-     * @param deck deck where the card will be added
+     * Adds the card to the given list and given index
+     *
+     * @param card  new card to be added
+     * @param list  list where the card will be added
      * @param index position where the card will be added
      */
-    public void addCard(Card card, ArrayList<Card> deck, int index) {
-        deck.add(index, card);
+    public void addCard(Card card, ArrayList<Card> list, int index) {
+        list.add(index, card);
     }
 
     /**
-     * Removes a card from a deck and returns the card
-     * @param deck deck where the card will be removed
-     * @param index position where the card to be removed is currently
-     * @return card removed
+     * Removes the card on the given index from the list and replaced by a card from the given deck
+     *
+     * @param list  list of cards
+     * @param index position of the card to remove
+     * @param deck  deck containing the card that replaces the old one
+     * @return removed card
      */
-    public Card removeCard(ArrayList<Card> deck, int index) {
-        return deck.remove(index);
+    private Card removeFromTable(ArrayList<Card> list, int index, Deck deck) {
+        Card toRemove = null;
+        if (list.get(index) != null) {
+            if (!deck.getCards().isEmpty())
+                toRemove = list.set(index, deck.removeCard());
+            else
+                toRemove = list.set(index, null);
+        } else {
+            // TODO gestire quando la posizione selezionata non contiene una carta
+        }
+        return toRemove;
+    }
+
+    /**
+     * Removes the selected card from the table and returns it
+     *
+     * @return removed card
+     */
+    public Card removeSelectedCard() {
+        Card toRemove = null;
+        if (selectedY == 0) {
+            if (selectedX == 2) {
+                if (!resourceDeck.getCards().isEmpty())
+                    toRemove = resourceDeck.removeCard();
+                else {
+                    // TODO gestire quando il mazzo è finito
+                }
+            } else {
+                toRemove = removeFromTable(resourceCards, selectedX, resourceDeck);
+            }
+        } else if (selectedY == 1) {
+            if (selectedX == 2) {
+                if (!goldDeck.getCards().isEmpty())
+                    toRemove = goldDeck.removeCard();
+                else {
+                    // TODO gestire quando il mazzo è finito
+                }
+            } else {
+                toRemove = removeFromTable(goldCards, selectedX, goldDeck);
+            }
+        }
+        return toRemove;
     }
 
     /**
      * Returns the card selected by the player
+     *
      * @return selected card
      */
     public Optional<Card> getSelectedCard() {
-        return Optional.ofNullable(this.selectedCard);
+        Card selectedCard = null;
+        if (selectedY == 0) {
+            if (selectedX == 0) {
+                selectedCard = resourceCards.getFirst();
+            } else if (selectedX == 1) {
+                selectedCard = resourceCards.get(1);
+            } else if (selectedX == 2) {
+                selectedCard = resourceDeck.getTopCard();
+            }
+        } else if (selectedY == 1) {
+            if (selectedX == 0) {
+                selectedCard = goldCards.getFirst();
+            } else if (selectedX == 1) {
+                selectedCard = goldCards.get(1);
+            } else if (selectedX == 2) {
+                selectedCard = goldDeck.getTopCard();
+            }
+        }
+        return Optional.ofNullable(selectedCard);
     }
 
     /**
      * Returns the Resources cards visible
+     *
      * @return resource cards visible
      */
     public ArrayList<Card> getResourceCards() {
@@ -108,6 +187,7 @@ public class CommonTable {
 
     /**
      * Returns the Gold cards visible
+     *
      * @return gold cards visible
      */
     public ArrayList<Card> getGoldCards() {
@@ -116,6 +196,7 @@ public class CommonTable {
 
     /**
      * Return the common missions
+     *
      * @return common mission
      */
     public ArrayList<Card> getCommonMissions() {
@@ -124,6 +205,7 @@ public class CommonTable {
 
     /**
      * Returns the resource cards deck
+     *
      * @return resource cards deck
      */
     public Deck getResourceDeck() {
@@ -132,6 +214,7 @@ public class CommonTable {
 
     /**
      * Returns the gold cards deck
+     *
      * @return gold cards deck
      */
     public Deck getGoldDeck() {
@@ -140,6 +223,7 @@ public class CommonTable {
 
     /**
      * Returns the initial cards deck
+     *
      * @return initial cards deck
      */
     public Deck getStarterDeck() {
@@ -148,9 +232,28 @@ public class CommonTable {
 
     /**
      * Returns the mission cards deck
+     *
      * @return mission cards deck
      */
     public Deck getMissionDeck() {
         return missionDeck;
+    }
+
+    /**
+     * Returns the selected card's X coordinate
+     *
+     * @return selectedX
+     */
+    public int getSelectedX() {
+        return selectedX;
+    }
+
+    /**
+     * Returns the selected card's Y coordinate
+     *
+     * @return selectedY
+     */
+    public int getSelectedY() {
+        return selectedY;
     }
 }
