de1d1c0c8dc6446b22be316f119c3bc3245087fb
karan <karanbir.jsm@gmail.com>
2024-04-18 18:15:51 +0200

    Add comments

--- src/main/java/it/polimi/ingsw/gc26/MainServer.java
+++ src/main/java/it/polimi/ingsw/gc26/MainServer.java
@@ -11,20 +11,21 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class MainServer {
     public static void main(String[] args) {
-
         // Start RMI Server
         final String serverName = "RMIMainController";
 
         try {
-            //publication and binding of the virtualMainController in the registry
+            // Create virtual main controller, the remote object
             System.out.println("Constructing server implementation...");
-            MainController mainController = new MainController();
-            System.out.println("Constructing server implementation...");
-
-            VirtualMainController virtualMainController = new VirtualRMIMainController(mainController);
+            VirtualMainController virtualMainController = new VirtualRMIMainController(new MainController());
             System.out.println("Binding server implementation to registry...");
+
+            // Create registry
             Registry registry = LocateRegistry.createRegistry(1099);
+
+            // Bind main controller
             registry.rebind(serverName, virtualMainController);
+            System.out.println("Server RMI on listening...");
         } catch (RemoteException e) {
             e.printStackTrace();
         }
