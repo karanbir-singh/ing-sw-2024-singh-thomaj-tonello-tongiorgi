5c3a59c3e50fd90c72217706fb63735d2a2063d7
karan <karanbir.jsm@gmail.com>
2024-04-20 23:04:32 +0200

    Fix setSelectedCard

--- src/main/java/it/polimi/ingsw/gc26/model/hand/Hand.java
+++ src/main/java/it/polimi/ingsw/gc26/model/hand/Hand.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.gc26.model.hand;
+
 import it.polimi.ingsw.gc26.model.card.Card;
 import it.polimi.ingsw.gc26.model.card_side.Side;
 
@@ -23,9 +24,10 @@ public class Hand {
 
     /**
      * Initializes the hand for the player
+     *
      * @param c new cards in hand
      */
-    public Hand(ArrayList<Card> c){
+    public Hand(ArrayList<Card> c) {
         this.cards = c;
         this.selectedCard = null;
         this.selectedSide = null;
@@ -33,6 +35,7 @@ public class Hand {
 
     /**
      * Returns an optional with the selected card if present
+     *
      * @return selected card
      */
     public Optional<Card> getSelectedCard() {
@@ -41,35 +44,40 @@ public class Hand {
 
     /**
      * Sets the selected card to the parameter given
+     *
      * @param selectedCard new selected card
      */
-    public void setSelectedCard(Card selectedCard){
-        if(Optional.ofNullable(selectedCard).isPresent()){
+    public void setSelectedCard(Card selectedCard) {
+        if (selectedCard != null) {
             this.selectedCard = selectedCard;
             this.selectedSide = selectedCard.getFront();
+            // TODO notify view
+        } else {
+            // TODO notify view
         }
     }
 
     /**
      * Returns the selected side of the selected card
+     *
      * @return selected side
      */
-    public Optional<Side> getSelectedSide(){
+    public Optional<Side> getSelectedSide() {
         return Optional.ofNullable(this.selectedSide);
     }
 
     /**
      * Sets selected side to the opposite side if there is a selected card
      */
-    public void turnSide(){
+    public void turnSide() {
         Optional<Card> selectedCard = Optional.ofNullable(this.selectedCard);
-        if(selectedCard.isPresent()){
-            if(selectedCard.get().getFront().equals(selectedSide)){
+        if (selectedCard.isPresent()) {
+            if (selectedCard.get().getFront().equals(selectedSide)) {
                 this.selectedSide = selectedCard.get().getBack();
             } else {
                 this.selectedSide = selectedCard.get().getFront();
             }
-        }else{
+        } else {
             // TODO lancia eccezione di carta non selezionata
         }
 
@@ -77,9 +85,10 @@ public class Hand {
 
     /**
      * Removes the card given as parameter from the hand
+     *
      * @param card card to be removed
      */
-    public void removeCard(Card card){
+    public void removeCard(Card card) {
         cards.remove(card);
         selectedSide = null;
         selectedCard = null;
@@ -87,17 +96,35 @@ public class Hand {
 
     /**
      * Adds the card given as a parameter in the hand
+     *
      * @param card new card in hand
      */
-    public void addCard(Card card){
+    public void addCard(Card card) {
         cards.add(card);
     }
 
     /**
      * Returns all the card in the hand
+     *
      * @return cards
      */
-    public ArrayList<Card> getCards(){
+    public ArrayList<Card> getCards() {
         return cards;
+    }
+
+    /**
+     * Return the card in cardIndex position
+     *
+     * @param cardIndex index of the card
+     * @return card in cards at position cardIndex
+     */
+    public Card getCard(int leftLimit, int rightLimit, int cardIndex) {
+        // Check if the given index is correct
+        if (cardIndex >= leftLimit && cardIndex < rightLimit) {
+            // TODO notify view
+            return cards.get(cardIndex);
+        }
+        // TODO notify view
+        return null;
     }
 }
