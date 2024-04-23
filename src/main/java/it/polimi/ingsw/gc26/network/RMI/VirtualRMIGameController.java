713cdbb64a6b589a45f56e5c3fe67862587d62b9
KevinThomaj <kevin.thomaj02@gmail.com>
2024-04-23 22:01:20 +0200

    added one request and modified VirtualRMIGameController

--- src/main/java/it/polimi/ingsw/gc26/network/RMI/VirtualRMIGameController.java
+++ src/main/java/it/polimi/ingsw/gc26/network/RMI/VirtualRMIGameController.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.gc26.network.RMI;
 
 import it.polimi.ingsw.gc26.controller.GameController;
 import it.polimi.ingsw.gc26.network.VirtualGameController;
+import it.polimi.ingsw.gc26.request.*;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -18,56 +19,68 @@ public class VirtualRMIGameController implements VirtualGameController {
 
     @Override
     public void choosePawnColor(String color, String playerID) throws RemoteException {
-        this.gameController.choosePawnColor(color, playerID);
+        //this.gameController.choosePawnColor(color, playerID);
+        this.gameController.addRequest(new ChoosePawnColorRequest(color, playerID));
+
     }
 
     @Override
     public void selectSecretMission(int cardIndex, String playerID) throws RemoteException  {
-        this.gameController.selectSecretMission(cardIndex, playerID);
+        //this.gameController.selectSecretMission(cardIndex, playerID);
+        this.gameController.addRequest(new SelectSecretMissionRequest(cardIndex, playerID));
     }
 
     @Override
     public void setSecretMission(String playerID) throws RemoteException  {
-        this.gameController.setSecretMission(playerID);
+        //this.gameController.setSecretMission(playerID);
+        this.gameController.addRequest(new SetSecretMissionRequest(playerID));
     }
 
     @Override
     public void selectCardFromHand(int cardIndex, String playerID) throws RemoteException  {
-        this.gameController.selectCardFromHand(cardIndex, playerID);
+        //this.gameController.selectCardFromHand(cardIndex, playerID);
+        this.gameController.addRequest(new SelectCardFromHandRequest(cardIndex, playerID));
     }
 
     @Override
     public void turnSelectedCardSide(String playerID) throws RemoteException  {
-        this.gameController.turnSelectedCardSide(playerID);
+        //this.gameController.turnSelectedCardSide(playerID);
+        this.gameController.addRequest(new TurnSelectedSideRequest(playerID));
     }
 
     @Override
     public void selectPositionOnBoard(int selectedX, int selectedY, String playerID) throws RemoteException  {
-        this.gameController.selectPositionOnBoard(selectedX, selectedY, playerID);
+        //this.gameController.selectPositionOnBoard(selectedX, selectedY, playerID);
+        this.gameController.addRequest(new SelectPositionOnBoardRequest(selectedX, selectedY, playerID));
     }
 
     @Override
-    public void playCardFromHand(String playerID) throws RemoteException  {
-        this.gameController.playCardFromHand(playerID);
+    public void playCardFromHand(String playerID) throws RemoteException{
+        //this.gameController.playCardFromHand(playerID);
+        this.gameController.addRequest(new PlayCardFromHandRequest(playerID));
     }
 
     @Override
     public void selectCardFromCommonTable(int cardX, int cardY, String playerID) throws RemoteException  {
-        this.gameController.selectCardFromCommonTable(cardX, cardY, playerID);
+        //this.gameController.selectCardFromCommonTable(cardX, cardY, playerID);
+        this.gameController.addRequest(new SelectCardFromCommonTableRequest(cardX, cardY,playerID));
     }
 
     @Override
     public void drawSelectedCard(String playerID) throws RemoteException  {
-        this.gameController.drawSelectedCard(playerID);
+        //this.gameController.drawSelectedCard(playerID);
+        this.gameController.addRequest(new DrawSelectedCardRequest(playerID));
     }
 
     @Override
     public void printPersonalBoard(String nickname, String playerID) throws RemoteException {
-        this.gameController.printPersonalBoard(nickname, playerID);
+        //this.gameController.printPersonalBoard(nickname, playerID);
+        this.gameController.addRequest(new PrintPersonalBoardRequest(nickname,playerID));
     }
 
     @Override
     public void addMessage(String line, String nicknameReceiver, String senderID, String time) throws RemoteException {
-        this.gameController.addMessage(line, nicknameReceiver, senderID, time);
+        //this.gameController.addMessage(line, nicknameReceiver, senderID, time);
+        this.gameController.addRequest(new AddMessageRequest(line,nicknameReceiver,senderID,time));
     }
 }
