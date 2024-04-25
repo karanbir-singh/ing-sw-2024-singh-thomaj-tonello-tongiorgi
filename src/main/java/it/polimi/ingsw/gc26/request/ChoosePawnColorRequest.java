f9bcb6a83e2eb843945440795f86d7b9b24a397a
karan <karanbir.jsm@gmail.com>
2024-04-25 13:02:02 +0200

    Fix mistake

--- src/main/java/it/polimi/ingsw/gc26/request/ChoosePawnColorRequest.java
+++ src/main/java/it/polimi/ingsw/gc26/request/ChoosePawnColorRequest.java
@@ -2,17 +2,17 @@ package it.polimi.ingsw.gc26.request;
 
 import it.polimi.ingsw.gc26.controller.GameController;
 
-public class ChoosePawnColorRequest implements Request{
+public class ChoosePawnColorRequest implements Request {
     final private String color;
     final private String playerID;
 
-    public ChoosePawnColorRequest(String color, String playerID){
+    public ChoosePawnColorRequest(String color, String playerID) {
         this.color = color;
         this.playerID = playerID;
     }
 
     @Override
     public void execute(GameController gameController) {
-        gameController.printPersonalBoard(this.color,this.playerID);
+        gameController.choosePawnColor(this.color, this.playerID);
     }
 }
