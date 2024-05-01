ae26460d24579ca4d15b4c89a7ca7246ac280dc6
karan <karanbir.jsm@gmail.com>
2024-05-01 11:31:05 +0200

    Add MainRequest for managing clients connection requests

---
+++ src/main/java/it/polimi/ingsw/gc26/request/main_request/GameCreationRequest.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.gc26.request.main_request;
+
+import it.polimi.ingsw.gc26.controller.MainController;
+import it.polimi.ingsw.gc26.network.VirtualView;
+
+public class GameCreationRequest implements MainRequest {
+    private VirtualView client;
+    private String nickname;
+    private int numberOfPlayers;
+    private int priority;
+
+    public GameCreationRequest(VirtualView client, String nickname, int numberOfPlayers, int priority) {
+        this.client = client;
+        this.nickname = nickname;
+        this.numberOfPlayers = numberOfPlayers;
+        this.priority = priority;
+    }
+
+    @Override
+    public void execute(MainController mainController) {
+        mainController.createWaitingList(client, nickname, numberOfPlayers);
+    }
+
+    @Override
+    public int getPriority() {
+        return this.priority;
+    }
+}
